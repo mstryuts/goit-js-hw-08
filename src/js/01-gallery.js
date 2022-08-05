27a44504217c6eed7da5aa3456cc35a382f3d229
maksim stryuts <maxim.stryuts@gmail.com>
2022-08-05 20:35:20 +0300

    ref

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
 
 const galleryContainerEl = document.querySelector(".gallery");
 const imagesMarkup = createItemsMarkup(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,13 +5,7 @@ const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 const STORAGE_KEY = "videoplayer-current-time";
 
+const onPlay = ({ seconds }) => localStorage.setItem(STORAGE_KEY, seconds);
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY))
-
-const whenPlay = ({seconds})=> {
-
-    localStorage.setItem(STORAGE_KEY, seconds); 
-
-};
-
-player.on('timeupdate', throttle(whenPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
