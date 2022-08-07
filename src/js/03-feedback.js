78cd86a1af94fa4f8ed24837ed3e96b194e1d48d
maksim stryuts <maxim.stryuts@gmail.com>
2022-08-07 16:35:44 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,38 @@
 import throttle from 'lodash.throttle';
 
 const refs = {
-    form : document.querySelector('.feedback-form'),
-    input : document.querySelector('.feedback-form input'),
-    message : document.querySelector('.feedback-form textarea'),
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea')
+}
+const STORAGE_VALUE = 'feedback-form-state'
+const formData = {};
+
+restoreData()
+
+function onFormSubmit(e) {
+    if (refs.input.value === "" || refs.message.input === "") {
+        return alert('Заполните все поля')
+    } else {
+        e.preventDefault();
+        e.target.reset();
+        localStorage.removeItem(STORAGE_VALUE)
+    }
 }
 
-const formData = {};
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    const stringData = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_VALUE, stringData);
+}
+
+function restoreData() {
+    const restData = JSON.parse(localStorage.getItem(STORAGE_VALUE));
+
+    if (restData) {
+        refs.input.value = restData.email || '';
+        refs.message.value = restData.message || '';
+    }
+}
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
